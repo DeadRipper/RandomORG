95fee6e3fe8124aeea1993609b67bf3a8065daa9
DeadRipper <hardyextrim@gmail.com>
2017-10-16 22:01:27 +0300

    Add files via upload

---
+++ RandomORG/RandomORG/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace RandomORG
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void generatebuton_Click(object sender, RoutedEventArgs e)
+        {
+
+            if (mintext.Text.Length > maxtext.Text.Length)
+                {
+                    MessageBox.Show("Напишите число по меньше");
+                }
+        }
+        private void mintext_KeyPress(object sender, KeyPressEventArgs e)
+            {
+            if ((e.KeyChar <= 48 || e.KeyChar >= 59) && e.KeyChar != 8)
+                e.Handled = true;
+        }
+    }
+}
