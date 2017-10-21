4f9443cba64d1e2a07f1a6cf9a1554de31e64272
DeadRipper <hardyextrim@gmail.com>
2017-10-21 23:02:15 +0300

    Add files via upload

--- RandomORG/RandomORG/MainWindow.xaml.cs
+++ RandomORG/RandomORG/MainWindow.xaml.cs
@@ -23,48 +23,12 @@ namespace RandomORG
             InitializeComponent();
         }
 
-        private void generatebuton_Click(object sender, RoutedEventArgs e)
+        private void digitbutton_Click(object sender, RoutedEventArgs e)
         {
-            Random r = new Random();
-            restext.Text = (r.Next(Convert.ToInt32(maxtext.Text) - (Convert.ToInt32(minimum.Text)) + 1) + Convert.ToInt32(minimum.Text)).ToString();
-            if (minimum.Text.Equals(maxtext.Text))
-            {
-                MessageBox.Show("Vvedite chislo menshe max");
-            }
+            Program window = new Program();
+            this.Close();
+            window.ShowDialog();
         }
-
-        private void mintext_TextChanged(object sender, TextCompositionEventArgs e)
-        {
-            /*try
-            {
-                Convert.ToInt32(minimum.Text);
-                MessageBox.Show("Vvedite chislo");
-            }
-            catch (FormatException)
-            {
-                MessageBox.Show("Vvedenno ne chislo");
-            }
-            minimum.Clear();*/
-        }
-
-        private void newbutton_Click(object sender, RoutedEventArgs e)
-        {
-            maxtext.Clear();
-            minimum.Clear();
-            restext.Clear();
-        }
-
-        private void generdec_Click(object sender, RoutedEventArgs e)
-        {
-            Random r = new Random();
-            resdec.Text = (r.NextDouble() * (Convert.ToDouble(maxdec.Text) - (Convert.ToDouble(mindec.Text))) + Convert.ToDouble(mindec.Text)).ToString();
-        }
-
-        private void decbutton_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
     }
 
 }
---
+++ RandomORG/RandomORG/Program.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace RandomORG
+{
+    /// <summary>
+    /// Логика взаимодействия для Menu.xaml
+    /// </summary>
+    public partial class Program : Window
+    {
+        public Program()
+        {
+            InitializeComponent();
+        }
+
+        private void generatebuton_Click(object sender, RoutedEventArgs e)
+        {
+            Random r = new Random();
+            restext.Text = (r.Next(Convert.ToInt32(maxtext.Text) - (Convert.ToInt32(minimum.Text)) + 1) + Convert.ToInt32(minimum.Text)).ToString();
+            if (minimum.Text.Equals(maxtext.Text))
+            {
+                MessageBox.Show("Vvedite chislo menshe max");
+            }
+        }
+
+        private void mintext_TextChanged(object sender, TextCompositionEventArgs e)
+        {
+            /*try
+            {
+                Convert.ToInt32(minimum.Text);
+                MessageBox.Show("Vvedite chislo");
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Vvedenno ne chislo");
+            }
+            minimum.Clear();*/
+        }
+
+        private void newbutton_Click(object sender, RoutedEventArgs e)
+        {
+            maxtext.Clear();
+            minimum.Clear();
+            restext.Clear();
+        }
+
+        private void generdec_Click(object sender, RoutedEventArgs e)
+        {
+            Random r = new Random();
+            resdec.Text = (r.NextDouble() * (Convert.ToDouble(maxdec.Text) - (Convert.ToDouble(mindec.Text))) + Convert.ToDouble(mindec.Text)).ToString();
+        }
+
+        private void decbutton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void backbutton_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow window = new MainWindow();
+            this.Close();
+            window.ShowDialog();
+        }
+    }
+}
